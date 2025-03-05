83053837b9adef4dd1b4a38d583a75c436e6b488
Israt <cpicstlab20@gamil.com>
2025-03-05 13:22:19 +0600

    update registion from

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <div className="bg-green-700 h-50 w-80 flex justify-center items-center">
+      <div className="bg-red-900 h-25 w-25 rounded-full">
+
+      </div>
+    </div>
+    <div className="h-90 w-2 bg-amber-900">
+
+    </div>
+     
+    </>
+  )
+}
+
+export default App
